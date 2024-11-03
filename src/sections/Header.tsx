820af36f2b0acadc27b3992898c325822b4f7327
Mateen Chowdhary <mateennazir0@gmail.com>
2024-11-03 14:48:03 +0530

    Header Component Done.
    For all views

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,13 +1,24 @@
 export const Header = () => {
   return (
     <div className="flex justify-center items-center relative top-3">
-      <nav>
-        <a href="#" className="px-4 py-1.5">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        <a href="#" className="nav-item">
           Home
         </a>
-        <a href="#">Projects</a>
-        <a href="#"> About</a>
-        <a href="#"> Contact</a>
+        <a href="#" className="nav-item">
+          Projects
+        </a>
+        <a href="#" className="nav-item">
+          {" "}
+          About
+        </a>
+        <a
+          href="#"
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+        >
+          {" "}
+          Contact
+        </a>
       </nav>
     </div>
   );
