64b609225dc0927ab2d37c635cbffb35978985e1
Mateen Chowdhary <mateennazir0@gmail.com>
2024-11-05 16:27:15 +0530

    Worked on background and animation with stars.
    figuring out how to rotate the stars in the orbit..

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,26 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
+import { HeroOrbit } from "@/components/HeroOrbit";
+
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      ></div>
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
+      <HeroOrbit size={800} rotation={-72}>
+        <StarIcon className="size-28 text-emerald-300" />
+      </HeroOrbit>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
@@ -18,15 +35,17 @@ export const HeroSection = () => {
             </div>
           </div>
         </div>
-        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
-          Building Exceptional User Experiences
-        </h1>
-        <p className="mt-4 text-center text-white/60">
-          {" "}
-          I specialize in trandforming designs into functional, high-performing
-          web applications.Lets discuss your next project
-        </p>
-        <div className=" flex flex-col items-center mt-8 gap-4">
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            {" "}
+            I specialize in trandforming designs into functional,
+            high-performing web applications.Lets discuss your next project
+          </p>
+        </div>
+        <div className=" flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
             {" "}
             <span className="font-semibold"> Explore my work</span>
