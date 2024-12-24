b5476bbdabc6b83c5a3c29449666f8660c6942f9
Mateen Chowdhary <mateennazir0@gmail.com>
2024-12-24 16:15:53 +0530

    Dome with the map card. Moving to the tablet view of the design

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -145,9 +145,19 @@ export const AboutSection = () => {
               ))}
             </div>
           </Card>
-          <Card>
-            <Image src={mapImage} alt="map" />
-            <Image src={smileMemoji} alt="smiling memoji" />
+          <Card className="h-[320px] p-0 relative">
+            <Image
+              src={mapImage}
+              alt="map"
+              className="h-full w-full object-cover object-left-top"
+            />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:insert-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+              <Image
+                src={smileMemoji}
+                alt="smiling memoji"
+                className="size-20"
+              />
+            </div>
           </Card>
         </div>
       </div>
