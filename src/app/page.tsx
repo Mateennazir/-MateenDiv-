17d7dc485d0061c5b28b683e3f56dada052e625c
Mateen Chowdhary <mateennazir0@gmail.com>
2024-12-25 16:43:05 +0530

    Done With the Design for all Views. Next is the Animation Part that's the only part left.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer";
 export default function Home() {
   return (
     <div>
@@ -15,6 +16,7 @@ export default function Home() {
       <TestimonialsSection />
       <AboutSection />
       <ContactSection />
+      <Footer />
     </div>
   );
 }
