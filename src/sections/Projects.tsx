a7b0aa24e71fe668685f359c1f8995c2469b855d
Mateen Chowdhary <mateennazir0@gmail.com>
2024-12-26 14:19:53 +0530

    implemented sticky effect on project Section. Moving towards  the Tape.

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -57,10 +57,13 @@ export const ProjectsSection = () => {
         />
 
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, projectIndex) => (
             <Card
               key={project.title}
-              className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 "
+              className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px`,
+              }}
             >
               <div className="lg:grid lg:grid-cols-2 gap-16">
                 <div className="lg:pb-16">
