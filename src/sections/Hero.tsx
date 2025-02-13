e9085880ec3ec392ac1e04fd513bee39d5ece798
Mateen Chowdhary <mateennazir0@gmail.com>
2025-02-13 20:58:10 +0530

    some changes

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -143,7 +143,7 @@ export const HeroSection = () => {
             <ArrowDown className=" size-4" />
           </button>
 
-          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl ">
             <a href="mailto:muteebnazir8@gmail.com">
               <span>👋</span>
               <span className="font-semibold">Lets connect</span>
